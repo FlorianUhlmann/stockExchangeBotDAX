269b8c3a4b6cb8197a604e09350c76c0b1796fb3
UHLMANN Florian <florian.uhlmann@soprasteria.com>
2020-03-30 11:51:23 +0200

    TDD(not working properly!!) : TODO - two tests exist - first test runs, BUT seems to be independend of code to be tested - second test fails, TODO make test green

--- Fruehstueck-DL/Fruehstueck_DL_DataPreparation_for_DAX_v1_1_TDD.py
+++ Fruehstueck-DL/Fruehstueck_DL_DataPreparation_for_DAX_v1_1_TDD.py
@@ -3,7 +3,7 @@ import pandas as pd
 from pandas.util.testing import assert_frame_equal
 import datetime as h
 
-from Fruehstueck_DL_DataPreparation_for_DAX_v1_1 import DataPreparation
+from Fruehstueck_DL_DataPreparation_for_DAX_v1_1 import DataPreparationDAX
 
 class TestDataPreparation(unittest.TestCase):
 
@@ -15,7 +15,7 @@ class TestDataPreparation(unittest.TestCase):
         SollDataFrame = pd.DataFrame({'Hello': []}, columns=['Hello'])
         SollDataFrame = SollDataFrame.astype(object)
         #get dataframe to check
-        dataPreparation = DataPreparation('d:/Profiles/fuhlmann/Programmierung/Python/boerse_DataScience_project/Boersendaten/testCSV.csv')
+        dataPreparation = DataPreparationDAX('d:/Profiles/fuhlmann/Programmierung/Python/boerse_DataScience_project/Boersendaten/testCSV.csv')
         ISTDataFrame = dataPreparation.dataFrame
         #compare both dataframes
         assert_frame_equal(ISTDataFrame, SollDataFrame, check_column_type = True, check_frame_type = False, check_index_type = False)
@@ -26,15 +26,15 @@ class TestDataPreparation(unittest.TestCase):
         SollDataFrame = pd.DataFrame({'date':[h.date(2019,1,2),h.date(2019,1,2),h.date(2019,1,2),h.date(2019,1,2),h.date(2019,1,3),h.date(2019,1,3),h.date(2019,1,3),h.date(2019,1,4),h.date(2019,1,4)],
                                         'time': [h.time(22,59),h.time(9,00),h.time(10),h.time(12),h.time(22,59),h.time(9,00),h.time(10),h.time(12),h.time(22,59)],
                                         'close':[1000.0,1200.0,1400.0,1600.0,1000.0,1200.0,1400.0,1600.0,1000.0]})
-        print(SollDataFrame)
-        DFtoPrepare = DataPreparation(
+        DFtoPrepare = DataPreparationDAX(
             'D:/Profiles/fuhlmann/Programmierung/Python/boerse_DataScience_project/Boersendaten/DAX30_TimeFrameMin_M1_CandleData_TDD_DataFrameMuster.csv')
-        # DFtoPrepare.dataframe
         DFtoPrepare.dataFrame = DFtoPrepare.removeDataInTimerange(DFtoPrepare.dataFrame)
-        DFtoPrepare.dataFrame = DFtoPrepare.removeColumns(DFtoPrepare.dataFrame)
-        DFtoPrepare.dataFrame = DFtoPrepare.removeColumns(DFtoPrepare.dataFrame)
-        DFtoPrepare.dataFrames = DFtoPrepare.dataFrame.iloc[9:]
         DFtoPrepare.showDataFrame()
+        DFtoPrepare.dataFrame = DFtoPrepare.keepOnlyColumnsDateTimeClose(DFtoPrepare.dataFrame)
+        ISTDataFrame = DFtoPrepare.dataFrame
+        #DFtoPrepare.dataFrame = DFtoPrepare.dataFrame.iloc[9:]
+        #DFtoPrepare.showDataFrame()
+        assert_frame_equal(ISTDataFrame, SollDataFrame, check_column_type = False, check_frame_type = False, check_index_type = False)
 
 
 if __name__ == '__main__':

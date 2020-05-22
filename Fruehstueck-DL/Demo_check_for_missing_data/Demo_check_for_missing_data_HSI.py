190870669ff1c83abd8ba2c94cc1a4269c237849
UHLMANN Florian <florian.uhlmann@soprasteria.com>
2020-05-22 19:50:09 +0200

    chore : committing despite nothing was changed in code since last commit..strange

--- Fruehstueck-DL/Demo_check_for_missing_data/Demo_check_for_missing_data_HSI.py
+++ Fruehstueck-DL/Demo_check_for_missing_data/Demo_check_for_missing_data_HSI.py
@@ -8,7 +8,7 @@ class DemoCheckForMissingDataHSI:
     def __init__(self):
         pass
 
-    def seachAndReplaceMissingDataHSI(self,inputDataFrame,DAX_list_tradingTimes):
+    def seachAndReplaceMissingDataHSI(self,inputDataFrame,HSI_list_tradingTimes):
 
         df = inputDataFrame
 
@@ -20,11 +20,11 @@ class DemoCheckForMissingDataHSI:
                 return inc
 
         def checkCorrectDataFrameEnding(inc_index, lengthDataFrame, df_incrt, inx, inc, DAX_list_Time):
-
+            print("checkCorrectDataFrameEnding")
             if (df_incrt.time[inx] == 10):
                 print("is correct end")
                 df_incrt
-            elif (df_incrt.time[inx] == 9):
+            elif (df_incrt.time[inx] != 10):
                 rowToAppend = pd.DataFrame(
                     {"date": df_incrt.date.loc[inx], "time": DAX_list_Time[inc], "close": df_incrt.close.loc[inx]},
                     index=[inx])
@@ -42,34 +42,64 @@ class DemoCheckForMissingDataHSI:
             for inx, curTime in enumerate(df.time):
                 inc = resetListIndex(inx, inc)
 
-                if curTime == DAX_list_tradingTimes[inc]:
+                #if value of df.time as expected
+                if curTime == HSI_list_tradingTimes[inc]:
                     inc_index = inx + 1
-
+                #else change value
                 else:
-
-                    if (inx % 9 == 0 and inx != 0):
+                    #if the value to change is the last value in HSI_list_tradingTimes AND indexvalue for the value is under 10
+                    if (inx % 9 == 0 and inx != 0 and HSI_list_tradingTimes[inc] == 10):
                         # create value
-                        rowToInsert = pd.DataFrame({"date": df.date.loc[inx - 1], "time": DAX_list_tradingTimes[inc],
+                        print('in row 1')
+                        rowToInsert = pd.DataFrame({"date": df.date.loc[inx - 1], "time": HSI_list_tradingTimes[inc],
                                                     "close": df.close.loc[inx - 1]}, index=[inx])
                         # insert value
                         df = pd.concat([df.iloc[:inx], rowToInsert, df.iloc[inx:]]).reset_index(drop=True)
+                        #update indices
                         inc_index = inx + 1
                         inc = 0
                         lengthDataFrame = len(df)
                         break
-                    else:
-                        rowToInsert = pd.DataFrame(
-                            {"date": df.date.loc[inx], "time": DAX_list_tradingTimes[inc], "close": df.close.loc[inx]},
-                            index=[inx])
+                    # if the value to change is the last value in HSI_list_tradingTimes AND indexvalue for the value is over 10
+                    elif (inx % 19 == 0 and inx != 0 and HSI_list_tradingTimes[inc] == 10):
+                        # create value
+                        rowToInsert = pd.DataFrame({"date": df.date.loc[inx - 1], "time": HSI_list_tradingTimes[inc],
+                                                    "close": df.close.loc[inx - 1]}, index=[inx])
+                        # insert value
                         df = pd.concat([df.iloc[:inx], rowToInsert, df.iloc[inx:]]).reset_index(drop=True)
+                        #update indices
+                        inc_index = inx + 1
+                        inc = 0
+                        lengthDataFrame = len(df)
+                        break
+                    # if the value is not the last value in     HSI_list_tradingTimes
+                    else:
+                        # handle special case -> value looks like last value before row starts again, but is just
+                        #  bc of missing data   e.g. 1,2,3,4,5,6,1,2,3,4,5.....(missing7,8,9,10)
+                        if (df.date.loc[inx - 1] != df.date.loc[inx] == df.date.loc[inx + 1] and inc != 0):
+                           #create missing row
+                            rowToInsert = pd.DataFrame({"date": df.date.loc[inx - 1], "time": HSI_list_tradingTimes[inc],
+                                 "close": df.close.loc[inx - 1]}, index=[inx])
+                        #handle normal case  e.g.  1,2,3,4, 7,8,9,10,1,2,3...... (missing 5,6)
+                        else:
+                            #create missing row
+                            rowToInsert = pd.DataFrame({"date": df.date.loc[inx], "time": HSI_list_tradingTimes[inc],
+                                                        "close": df.close.loc[inx]}, index=[inx])
+                        #insert missing row
+                        df = pd.concat([df.iloc[:inx], rowToInsert, df.iloc[inx:]]).reset_index(drop=True)
+                        #update indices
+                        inc_index = inx + 1
+                        inc = 0
+                        lengthDataFrame = len(df)
+                        break
 
+                inc = inc + 1
+                #if end of datalist
+                if (inc_index == lengthDataFrame):
+                    
+                    df = checkCorrectDataFrameEnding(inc_index, lengthDataFrame, df, inx, inc, HSI_list_tradingTimes)
+                    #update indices
                     inc_index = inx + 1
                     inc = 0
                     lengthDataFrame = len(df)
-                    break
-
-                inc = inc + 1
-                if (inc_index == lengthDataFrame):
-                    df = checkCorrectDataFrameEnding(inc_index, lengthDataFrame, df, inx, inc, DAX_list_tradingTimes)
-
         return df
--- Fruehstueck-DL/Demo_check_for_missing_data/Demo_check_for_missing_data_HSI_TDD.py
+++ Fruehstueck-DL/Demo_check_for_missing_data/Demo_check_for_missing_data_HSI_TDD.py
@@ -1,7 +1,7 @@
 import os
 import unittest
 import pandas as pd
-from pandas.util.testing import assert_frame_equal
+from pandas.testing import assert_frame_equal
 
 from Demo_check_for_missing_data_HSI import *
 
@@ -48,17 +48,57 @@ class MissingValuesInDataFrames(unittest.TestCase):
             df_data_correct_SOLL = pd.DataFrame({'date': dateData, 'time': timeData, 'close': valueData})
             return df_data_correct_SOLL
 
-        def createDFSOLL_testCase_onePlaceFalseAtEndOfListCorrectTime():
+        def createDFSOLL_testCase_MissingPlacesAtEndOfListCorrectTime():
             dateOne = ['2020-01-02', '2020-01-02', '2020-01-02', '2020-01-02', '2020-01-02', '2020-01-02',
                        '2020-01-02', '2020-01-02', '2020-01-02', '2020-01-02', '2020-01-03', '2020-01-03', '2020-01-03',
-                       '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-03']
-            dateData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+                       '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-03',
+                       '2020-01-04', '2020-01-04', '2020-01-04', '2020-01-04',
+                       '2020-01-04', '2020-01-04', '2020-01-04', '2020-01-04', '2020-01-04', '2020-01-04']
+            dateData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
             valueData = [1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 1009, 1011, 1012, 1013, 1014, 1015, 1016,
-                         1017, 1018, 1019, 1019]
+                         1017, 1018, 1019, 1019, 2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2029]
             df_data_correct_SOLL_last_place_missing = pd.DataFrame(
                 {'date': dateOne, 'time': dateData, 'close': valueData})
             return df_data_correct_SOLL_last_place_missing
-            
+
+        def createDFSOLL_testCase_MissingPlacesAtEndOfListCorrectTimeAndLast3Values():
+            dateOne = ['2020-01-02', '2020-01-02', '2020-01-02', '2020-01-02', '2020-01-02', '2020-01-02',
+                       '2020-01-02', '2020-01-02', '2020-01-02', '2020-01-02', '2020-01-03', '2020-01-03', '2020-01-03',
+                       '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-03',
+                       '2020-01-04', '2020-01-04', '2020-01-04', '2020-01-04',
+                       '2020-01-04', '2020-01-04', '2020-01-04', '2020-01-04', '2020-01-04', '2020-01-04']
+            dateData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+            valueData = [1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 1009, 1011, 1012, 1013, 1014, 1015, 1016,
+                         1017, 1018, 1019, 1019, 2021, 2022, 2023, 2024, 2025, 2026, 2026, 2026, 2026, 2026]
+            df_data_correct_SOLL_last_place_missing = pd.DataFrame(
+                {'date': dateOne, 'time': dateData, 'close': valueData})
+            return df_data_correct_SOLL_last_place_missing
+
+        def createDFSOLL_testCase_Missing9PlacesAtIndex4and8a16a17a18a21a22a23a24():
+            dateOne = ['2020-01-02', '2020-01-02', '2020-01-02', '2020-01-02', '2020-01-02', '2020-01-02',
+                       '2020-01-02', '2020-01-02', '2020-01-02', '2020-01-02', '2020-01-03', '2020-01-03', '2020-01-03',
+                       '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-03',
+                       '2020-01-04', '2020-01-04', '2020-01-04', '2020-01-04',
+                       '2020-01-04', '2020-01-04', '2020-01-04', '2020-01-04', '2020-01-04', '2020-01-04']
+            dateData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+            valueData = [1001, 1002, 1003, 1004, 1006, 1006, 1007, 1008, 1009, 1009, 1011, 1012, 1013, 1014, 1015, 1016,
+                         1017, 1018, 1018, 1018, 2022, 2022, 2023, 2024, 2025, 2026, 2026, 2026, 2026, 2026]
+            df_data_correct_SOLL__missing_9_values = pd.DataFrame(
+                {'date': dateOne, 'time': dateData, 'close': valueData})
+            return df_data_correct_SOLL__missing_9_values
+
+        def createDFSOLL_testCase_Missing12Places():
+            dateOne = ['2020-01-02', '2020-01-02', '2020-01-02', '2020-01-02', '2020-01-02', '2020-01-02',
+                       '2020-01-02', '2020-01-02', '2020-01-02', '2020-01-02', '2020-01-03', '2020-01-03', '2020-01-03',
+                       '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-03',
+                       '2020-01-04', '2020-01-04', '2020-01-04', '2020-01-04',
+                       '2020-01-04', '2020-01-04', '2020-01-04', '2020-01-04', '2020-01-04', '2020-01-04']
+            dateData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+            valueData = [1001, 1002, 1003, 1004, 1006, 1006, 1007, 1008, 1009, 1009, 1011, 1012, 1013, 1016, 1016, 1016,
+                         1017, 1018, 1018, 1018, 2023, 2023, 2023, 2024, 2025, 2026, 2026, 2026, 2026, 2026]
+            df_data_correct_SOLL_missing_12_values = pd.DataFrame({'date': dateOne, 'time': dateData, 'close': valueData})
+            return df_data_correct_SOLL_missing_12_values
+
         def createListCorrectTime():
             list_time = [1,2,3,4,5,6,7,8,9,10]
             return list_time
@@ -97,16 +137,54 @@ class MissingValuesInDataFrames(unittest.TestCase):
             df_data_incorrect = pd.DataFrame({'date': dateData, 'time': timeData, 'close': valueData})
             return df_data_incorrect
 
-        def createDFIncorrectOnePlaceFalseAtEndOfListCorrectTime():
+        def createDFIncorrectMissingPlacesAtEndOfListCorrectTime():
             dateOne = ['2020-01-02', '2020-01-02', '2020-01-02', '2020-01-02', '2020-01-02', '2020-01-02',
                        '2020-01-02', '2020-01-02', '2020-01-02', '2020-01-03', '2020-01-03', '2020-01-03',
-                       '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-03']
-            dateData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+                       '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-03',
+                       '2020-01-04', '2020-01-04', '2020-01-04',
+                       '2020-01-04', '2020-01-04', '2020-01-04', '2020-01-04', '2020-01-04', '2020-01-04']
+            dateData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9]
             valueData = [1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 1011, 1012, 1013, 1014, 1015, 1016,
-                         1017, 1018, 1019]
+                         1017, 1018, 1019, 2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029]
             df_data_INcorrect_missing_last_place_from_list = pd.DataFrame(
                 {'date': dateOne, 'time': dateData, 'close': valueData})
             return df_data_INcorrect_missing_last_place_from_list
+
+        def createDFIncorrectMissingPlacesFalseAtEndOfListCorrectTimeANDlast3values():
+            dateOne = ['2020-01-02', '2020-01-02', '2020-01-02', '2020-01-02', '2020-01-02', '2020-01-02',
+                       '2020-01-02', '2020-01-02', '2020-01-02', '2020-01-03', '2020-01-03', '2020-01-03',
+                       '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-03',
+                       '2020-01-04', '2020-01-04', '2020-01-04',
+                       '2020-01-04', '2020-01-04', '2020-01-04', ]
+            dateData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, ]
+            valueData = [1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 1011, 1012, 1013, 1014, 1015, 1016,
+                         1017, 1018, 1019, 2021, 2022, 2023, 2024, 2025, 2026]
+            df_data_INcorrect_missing_last_place_from_list_and_last_3_values = pd.DataFrame(
+                {'date': dateOne, 'time': dateData, 'close': valueData})
+            return df_data_INcorrect_missing_last_place_from_list_and_last_3_values
+
+
+        def createDFINcorrectMissing9PlacesAtIndex4and8a16a17a18a21a22a23a24():
+            dateOne = ['2020-01-02', '2020-01-02', '2020-01-02', '2020-01-02', '2020-01-02', '2020-01-02',
+                       '2020-01-02', '2020-01-02', '2020-01-03', '2020-01-03', '2020-01-03',
+                       '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-04', '2020-01-04',
+                       '2020-01-04', '2020-01-04', '2020-01-04', ]
+            dateData = [1, 2, 3, 4, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 2, 3, 4, 5, 6, ]
+            valueData = [1001, 1002, 1003, 1004, 1006, 1007, 1008, 1009, 1011, 1012, 1013, 1014, 1015, 1016,
+                         1017, 1018, 2022, 2023, 2024, 2025, 2026]
+            df_data_INcorrect_missing_9_values = pd.DataFrame({'date': dateOne, 'time': dateData, 'close': valueData})
+            return df_data_INcorrect_missing_9_values
+
+        def createDFINcorrectMissing12places():
+            dateOne = ['2020-01-02', '2020-01-02', '2020-01-02', '2020-01-02', '2020-01-02', '2020-01-02',
+                       '2020-01-02', '2020-01-02', '2020-01-03',
+                       '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-03', '2020-01-04',
+                       '2020-01-04', '2020-01-04', '2020-01-04', ]
+            dateData = [1, 2, 3, 4, 6, 7, 8, 9, 1, 2, 3, 6, 7, 8, 3, 4, 5, 6, ]
+            valueData = [1001, 1002, 1003, 1004, 1006, 1007, 1008, 1009, 1011, 1012, 1013, 1016,
+                         1017, 1018, 2023, 2024, 2025, 2026]
+            df_data_INcorrect_missing_12_values = pd.DataFrame({'date': dateOne, 'time': dateData, 'close': valueData})
+            return df_data_INcorrect_missing_12_values
 
         #create DataFrames
         self.DFSOLL_testCase_onePlaceFalse = createDFSOLL_testCase_onePlaceFalse()
@@ -118,21 +196,30 @@ class MissingValuesInDataFrames(unittest.TestCase):
         self.DFSOLL_testCase_twoConsecutivePlacesFalse = createDFSOLL_testCase_twoConsecutivePlacesFalse_plusOnePlaceFalse()
         self.DFIncorrectMissingTwoConsecutiveValuesAndOneValue = createDFIncorrectMissingTwoConsecutiveValuesAndOneValue()
 
-        self.DFSOLL_testCase_onePlaceFalseAtEndOfListCorrectTime = createDFSOLL_testCase_onePlaceFalseAtEndOfListCorrectTime()
-        self.DFIncorrectOnePlaceFalseAtEndOfListCorrectTime = createDFIncorrectOnePlaceFalseAtEndOfListCorrectTime()
+        self.DFSOLL_testCase_MissingPlacesAtEndOfListCorrectTime = createDFSOLL_testCase_MissingPlacesAtEndOfListCorrectTime()
+        self.DFIncorrectOnePlaceFalseAtEndOfListCorrectTime = createDFIncorrectMissingPlacesAtEndOfListCorrectTime()
+
+        self.DFSOLL_testCase_MissingPlacesAtEndOfListCorrectTimeAndLast3Values = createDFSOLL_testCase_MissingPlacesAtEndOfListCorrectTimeAndLast3Values()
+        self.DFIncorrectMissingPlacesFalseAtEndOfListCorrectTimeANDlast3values = createDFIncorrectMissingPlacesFalseAtEndOfListCorrectTimeANDlast3values()
+
+        self.DFSOLL_testCase_Missing9PlacesAtIndex4and8a16a17a18a21a22a23a24 = createDFSOLL_testCase_Missing9PlacesAtIndex4and8a16a17a18a21a22a23a24()
+        self.DFINcorrectMissing9PlacesAtIndex4and8a16a17a18a21a22a23a24 = createDFINcorrectMissing9PlacesAtIndex4and8a16a17a18a21a22a23a24()
+
+        self.DFSOLL_testCase_Missing12Places = createDFSOLL_testCase_Missing12Places()
+        self.DFINcorrectMissing12places = createDFINcorrectMissing12places()
 
         self.listCorrectTime = createListCorrectTime()
 
-    '''
+
     def test_find_one_missing_value_in_DataFrame_and_replace_it(self):
         SOLL_DataFrame = self.DFSOLL_testCase_onePlaceFalse
         DFIncorrectOnePlaceFalse = self.DFIncorrectOnePlaceFalse
 
-        CheckForMissingData = DemoCheckForMissingData()
-        IST_DataFrame = CheckForMissingData.seachAndReplaceMissingData(DFIncorrectOnePlaceFalse,self.listCorrectTime)
+        CheckForMissingData = DemoCheckForMissingDataHSI()
+        IST_DataFrame = CheckForMissingData.seachAndReplaceMissingDataHSI(DFIncorrectOnePlaceFalse,self.listCorrectTime)
 
         assert_frame_equal(SOLL_DataFrame,IST_DataFrame, check_dtype=False)
-        '''
+
 
     '''
     def test_find_two_missing_value_in_DataFrame_and_replace_it(self):
@@ -144,19 +231,28 @@ class MissingValuesInDataFrames(unittest.TestCase):
 
         assert_frame_equal(SOLL_DataFrame, IST_DataFrame, check_dtype=False)
     '''
-    '''
+
     def test_find_two_consecutive_places_missing__and_one_simple_missing_value_and_replace_them(self):
         DFIncorrect = self.DFIncorrectMissingTwoConsecutiveValuesAndOneValue
         SOLL_DataFrame = self.DFSOLL_testCase_twoConsecutivePlacesFalse
 
-        CheckForMissingData = DemoCheckForMissingData()
-        IST_DataFrame = CheckForMissingData.seachAndReplaceMissingData(DFIncorrect, self.listCorrectTime)
+        CheckForMissingData = DemoCheckForMissingDataHSI()
+        IST_DataFrame = CheckForMissingData.seachAndReplaceMissingDataHSI(DFIncorrect, self.listCorrectTime)
 
         assert_frame_equal(SOLL_DataFrame, IST_DataFrame, check_dtype=False)
-    '''
+
     def test_find_missing_data_on_last_place_of_list(self):
         DFIncorrect = self.DFIncorrectOnePlaceFalseAtEndOfListCorrectTime
-        SOLL_DataFrame = self.DFSOLL_testCase_onePlaceFalseAtEndOfListCorrectTime
+        SOLL_DataFrame = self.DFSOLL_testCase_MissingPlacesAtEndOfListCorrectTime
+
+        CheckForMissingData = DemoCheckForMissingDataHSI()
+        IST_DataFrame = CheckForMissingData.seachAndReplaceMissingDataHSI(DFIncorrect, self.listCorrectTime)
+
+        assert_frame_equal(SOLL_DataFrame, IST_DataFrame, check_dtype=False)
+
+    def test_find_missing_data_on_last_place_of_list_and_3_missing_values_at_the_end(self):
+        DFIncorrect = self.DFIncorrectMissingPlacesFalseAtEndOfListCorrectTimeANDlast3values
+        SOLL_DataFrame = self.DFSOLL_testCase_MissingPlacesAtEndOfListCorrectTimeAndLast3Values
 
         CheckForMissingData = DemoCheckForMissingDataHSI()
         IST_DataFrame = CheckForMissingData.seachAndReplaceMissingDataHSI(DFIncorrect, self.listCorrectTime)
@@ -167,6 +263,23 @@ class MissingValuesInDataFrames(unittest.TestCase):
         print(SOLL_DataFrame)
         assert_frame_equal(SOLL_DataFrame, IST_DataFrame, check_dtype=False)
 
+    def test_find_missing_data_missing_9_values(self):
+        DFIncorrect = self.DFINcorrectMissing9PlacesAtIndex4and8a16a17a18a21a22a23a24
+        SOLL_DataFrame = self.DFSOLL_testCase_Missing9PlacesAtIndex4and8a16a17a18a21a22a23a24
+
+        CheckForMissingData = DemoCheckForMissingDataHSI()
+        IST_DataFrame = CheckForMissingData.seachAndReplaceMissingDataHSI(DFIncorrect, self.listCorrectTime)
+
+        assert_frame_equal(SOLL_DataFrame, IST_DataFrame, check_dtype=False)
+
+    def test_find_missing_data_missing_12_values(self):
+        DFIncorrect = self.DFINcorrectMissing12places
+        SOLL_DataFrame = self.DFSOLL_testCase_Missing12Places
+
+        CheckForMissingData = DemoCheckForMissingDataHSI()
+        IST_DataFrame = CheckForMissingData.seachAndReplaceMissingDataHSI(DFIncorrect, self.listCorrectTime)
+
+        assert_frame_equal(SOLL_DataFrame, IST_DataFrame, check_dtype=False)
     def tearDown(self):
         self.DFSOLL_testCase_onePlaceFalse = None
         self.DFIncorrectOnePlaceFalse = None
